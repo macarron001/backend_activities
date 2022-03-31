456ec81a25237fe11e1421ba0847f74227406da5
Ronny <91857581+macarron001@users.noreply.github.com>
2022-03-31 15:35:09 +0800

    cleaning up folder

--- activity_1_1_4.rb
+++
@@ -1,6 +0,0 @@
-puts 'what is your age?'
-age = gets.to_i
-puts "In 10 years you will be #{age + 10}"
-puts "In 20 years you will be #{age + 20}"
-puts "In 30 years you will be #{age + 30}"
-puts "In 40 years you will be #{age + 40}"
