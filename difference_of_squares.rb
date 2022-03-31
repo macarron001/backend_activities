cfcf0acabf26ee32b3e3f070aee147033f8a7c5f
Ronny <91857581+macarron001@users.noreply.github.com>
2022-03-31 21:17:53 +0800

    assignment

---
+++ difference_of_squares.rb
@@ -0,0 +1,17 @@
+num = 1
+squared_array = []
+number_array = []
+while num <= 50
+    squared_array.push(num ** 2)
+    number_array.push(num)
+    num += 1
+end
+
+sum_of_squares = squared_array.map(&:to_i).reduce(0, :+)
+sum_of_numbers = number_array.map(&:to_i).reduce(0, :+)
+square_of_sum = sum_of_numbers**2
+difference_between = square_of_sum - sum_of_squares
+
+puts "The sum of the squares is #{sum_of_squares}"
+puts "The square of the sum #{square_of_sum}"
+puts "The difference is #{difference_between}"
