0c39d737712c25af3545c73f232f63cf133f580b
Ronny <91857581+macarron001@users.noreply.github.com>
2022-03-31 19:17:26 +0800

    included checker for single digit

--- square_of_squares.rb
+++ square_of_squares.rb
@@ -12,8 +12,15 @@ number = gets.chomp()
 non_perfect_square = ["2", "3", "7", "8"]
 perfect_square = [0,1,4,7,9]
 
+#if number is a single digit number
+if number.length == 1
+    if number == "4" || number == "9"
+        puts "#{number} => true"
+    else
+        puts "#{number} => false"
+    end
 #if last digit ends in a non perfect square digit
-if non_perfect_square.include? number[-1]
+elsif non_perfect_square.include? number[-1]
     puts "#{number} => false"
 
 #if instance of 0's in the number is odd
@@ -25,7 +32,8 @@ elsif number[-1] == "0"
 
 #if number ends in 6 and 2nd to last digit is odd    
 elsif number[-1] == "6"
-    if number[-2] % 2 != 0
+    if number.length == 1  
+    elsif number[-2] % 2 != 0
         if number.length == 3
             if number[-2] == "9"
                 puts "#{number} => true"
