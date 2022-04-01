c0355ff39e30758e51e982faf3d4504deacb3934
Ronny <91857581+macarron001@users.noreply.github.com>
2022-04-01 15:31:51 +0800

    refactored to case exp

--- calculator.rb
+++ calculator.rb
@@ -1,10 +1,3 @@
-# puts "Enter a number"
-# number1 = gets.chomp().to_i
-# puts "Enter another number"
-# number2 = gets.chomp().to_i
-# sum = number1 + number2
-# puts "The sum is #{sum}"
-
 puts "Hello! This is a calculator"
 puts "Enter the first number"
 number1 = gets.chomp().to_f
@@ -13,18 +6,42 @@ operator = gets.chomp()
 puts "Enter the second number"
 number2 = gets.chomp().to_f
 
-if operator == "+"
+case operator
+when "+"
     answer = number1 + number2
     puts "The sum is #{answer}"
-elsif operator == "-"
+when "-"
     answer = number1 - number2
     puts "The difference is #{answer}"
-elsif operator == "*"
+when "*"
     answer = number1 * number2
     puts "The product is #{answer}"
-elsif operator == "/"
+when "/"
     answer = number1 / number2
     puts "The quotient is #{answer}"
 else
     puts "Please put a valid operator"
 end
+
+# puts "Enter a number"
+# number1 = gets.chomp().to_i
+# puts "Enter another number"
+# number2 = gets.chomp().to_i
+# sum = number1 + number2
+# puts "The sum is #{sum}"
+
+# if operator == "+"
+#     answer = number1 + number2
+#     puts "The sum is #{answer}"
+# elsif operator == "-"
+#     answer = number1 - number2
+#     puts "The difference is #{answer}"
+# elsif operator == "*"
+#     answer = number1 * number2
+#     puts "The product is #{answer}"
+# elsif operator == "/"
+#     answer = number1 / number2
+#     puts "The quotient is #{answer}"
+# else
+#     puts "Please put a valid operator"
+# end
