5f7a1f7650077d2252060f640d92939019cb7807
Ronny <91857581+macarron001@users.noreply.github.com>
2022-04-01 15:13:37 +0800

    completed the calculator function

--- calculator.rb
+++ calculator.rb
@@ -1,6 +1,30 @@
-puts "Enter a number"
+# puts "Enter a number"
+# number1 = gets.chomp().to_i
+# puts "Enter another number"
+# number2 = gets.chomp().to_i
+# sum = number1 + number2
+# puts "The sum is #{sum}"
+
+
+puts "Enter the first number"
 number1 = gets.chomp().to_i
-puts "Enter another number"
+puts "Enter operator"
+operator = gets.chomp()
+puts "Enter the second number"
 number2 = gets.chomp().to_i
-sum = number1 + number2
-puts "The sum is #{sum}"
+
+if operator == "+"
+    answer = number1 + number2
+    puts "The sum is #{answer}"
+elsif operator == "-"
+    answer = number1 - number2
+    puts "The difference is #{answer}"
+elsif operator == "*"
+    answer = number1 * number2
+    puts "The product is #{answer}"
+elsif operator == "/"
+    answer = number1 / number2
+    puts "The quotient is #{answer}"
+else
+    puts "Please put a valid operator"
+end
