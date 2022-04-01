971cd1fa55879fb3c6fd6a534b013b0840a5efbb
Ronny <91857581+macarron001@users.noreply.github.com>
2022-04-01 17:33:33 +0800

    converted prog to function

--- smallest_integer.rb
+++ smallest_integer.rb
@@ -10,3 +10,19 @@ arr.each do |num|
 end
 
 puts "The smallest integer is #{smallest}"
+
+
+# arr = [34,15,88,2]
+arr = [34,-345,-1,100]
+
+def get_smallest(arr)
+    smallest = arr[0]
+    arr.each do |num|
+        if num < smallest
+            smallest = num
+        end
+    end
+    puts "The smallest integer is #{smallest}"
+end
+
+get_smallest(arr)
