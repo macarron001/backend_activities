ca4f214792dc6a84ae0fc841d41ec7c0cedbd362
Ronny <91857581+macarron001@users.noreply.github.com>
2022-04-05 21:22:59 +0800

    activity on classes

---
+++ class.rb
@@ -0,0 +1,241 @@
+# class Person
+#   def initialize(name, country, sex)
+#     @name = name
+#     @country = country
+#     @sex = sex
+#   end
+
+#   def introduce
+#     puts "Hi, I'm #{@name}. I am from #{@country}."
+#   end
+
+#   def flirt
+#     puts "Hey girl, I'm boy" if @sex == "male"
+#     puts "Hey boy, I'm girl" if @sex == "female"
+#   end
+# end
+
+# person1 = Person.new("Pwe", "Russia", "female")
+# person1.introduce
+# person1.flirt
+
+
+
+# class Profile
+#   attr_accessor :full_name, :age, :address, :work
+#   def initialize(full_name, age, address, work)
+#     @full_name = full_name
+#     @age = age
+#     @address = address
+#     @work = work
+#   end
+# end
+
+# my_profile = Profile.new('Juan', 18, 'Bulacan', 'Instructor')
+
+# puts my_profile.full_name
+# my_profile.full_name = 'Juan Cruz'
+
+# my_profile.age = 25
+# my_profile.work = 'Software Engineer'
+
+# puts my_profile.full_name
+# puts my_profile.age
+# puts my_profile.work
+# puts my_profile.address
+
+# class Animal
+#   attr_accessor :name
+#   def initialize(name)
+#     @name = name
+#   end
+#   def speak
+#     "Hello!"
+#   end
+# end
+
+# class GoodDog < Animal
+#   def speak
+#     super + " from GoodDog class"
+#   end
+# end
+
+# sparky = GoodDog.new("PWE")
+# puts sparky.speak
+# puts sparky.name
+
+# module Swimmable
+#   def swim
+#     "I'm swimming!"
+#   end
+# end
+
+# class Animal; end
+
+# class Fish < Animal
+#   include Swimmable
+# end
+
+# dory = Fish.new
+# puts dory.swim
+
+# module Language
+#   module Lakomaisip
+#     class Programming
+#       def info
+#         'tumpak'
+#       end
+#     end
+#   end
+  
+
+#   class Ruby < Lakomaisip::Programming
+#     def info
+#       "#{super} Ruby Programming Language"
+#     end
+#   end
+
+#   class Python
+#     def info
+#       puts "Python Programming Language"
+#     end
+#   end
+# end
+
+# ruby = Language::Ruby.new
+# puts ruby.info
+
+# class Person
+#   def initialize(age)
+#     @age = age
+#   end
+
+#   def age_after_1_yr
+#     puts get_age + 1
+#   end
+
+#   private
+#   def get_age
+#     @age
+#   end
+# end
+
+# ana = Person.new(25)
+# ana.age_after_1_yr
+
+# class Animal
+
+#   def initialize(age)
+#     @age = age
+#   end
+
+#   def <(animal)
+#     age < (animal.age)
+#   end
+
+#   protected
+
+#   def age
+#     @age
+#   end
+# end
+
+# fido = Animal.new(3)
+# cat = Animal.new(2)
+# puts fido < cat
+
+# class Confection
+#   def bake
+#     puts 'Baking at 350 degrees for 25 minutes'
+#   end
+# end
+
+# # CupCake  template
+# class CupCake < Confection
+#   def frost
+#     puts 'Applying frosting'
+#   end
+# end
+
+# # BananaCake  template
+# class BananaCake < Confection
+# end
+
+# confectionOne =  Confection.new
+# cupCakeOne =  CupCake.new
+# bananaCakeOne = BananaCake.new
+
+# confectionOne.bake
+# cupCakeOne.bake
+# cupCakeOne.frost
+# bananaCakeOne.bake
+
+
+module AdminPermisson
+  def edit_users_profile
+    puts "Admin updated all users profile"
+  end
+end
+
+module BuyerPermission
+  def buy
+    puts "Buyer has bought an item"
+  end
+end
+
+class User
+
+  def initialize(username, password, ip_address)
+    @username = username
+    @password = password
+    @ip_address = ip_address
+  end
+
+  def change_password=(password)
+    password_changer(password)
+    puts "Password changed!"
+  end
+
+  
+  protected
+  def login
+    puts "User logged in. IP address: #{@ip_address}"
+  end
+
+  def password_changer(password)
+    @password = password
+  end
+
+end
+
+
+class Admin < User
+  include AdminPermisson
+  def admin_login
+    self.login
+  end
+end
+
+
+class Buyer < User
+  include BuyerPermission
+  def buyer_login
+    self.login
+  end
+end
+
+
+
+## execute
+
+my_admin = Admin.new('avionuser', 'password', '127.0.0.1')
+my_admin.admin_login
+my_admin.edit_users_profile
+
+my_admin.change_password = 'new_password'
+
+buyer = Buyer.new('juan', 'password', '127.0.0.1')
+buyer.buyer_login
+buyer.buy
+
+buyer.change_password = 'new_password'
